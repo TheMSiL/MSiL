a0e4357295c4a20f08c6a6f56cd5c4fc52d2631d
TheMSiL <silka.maksim15@gmail.com>
2023-07-14 00:32:13 +0300

    add replace method to Navigate in wrong path

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 				<Route path='/about' element={<About />} />
 				<Route path='/portfolio' element={<Portfolio />} />
 				<Route path='/contact' element={<Contact />} />
-				<Route path='*' element={<Navigate to='/' />} />
+				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
 		</Router>
 	);
