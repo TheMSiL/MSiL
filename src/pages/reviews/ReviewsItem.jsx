5064bd84a5ec814b14e06fe440fb661811f5dbe1
TheMSiL <silka.maksim15@gmail.com>
2024-07-18 23:38:50 +0300

    fix reviews

--- src/pages/reviews/ReviewsItem.jsx
+++ src/pages/reviews/ReviewsItem.jsx
@@ -1,7 +1,7 @@
 import './reviews.css';
-import ava from '../../assets/review_avatar.png'
+import ava from '../../assets/review_avatar.png';
 
-const ReviewsItem = ({name, text}) => {
+const ReviewsItem = ({ name, text }) => {
 	return (
 		<div className='review_item'>
 			<div className='review_left'>
@@ -9,6 +9,7 @@ const ReviewsItem = ({name, text}) => {
 				<p className='review_name'>{name}</p>
 				<p>CUSTOMER</p>
 			</div>
+			<span className='span_mob'>“</span>
 			<div className='review_right'>
 				<span>“</span>
 				<p>{text}</p>
